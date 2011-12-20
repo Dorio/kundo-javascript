8279b1af6a002b8b2258564ae8e6a8dc5ad88a8c
Emil Stenström <em@kth.se>
2011-12-20 16:03:51 +0100

    Remove formatting parameter. JSON is the best choice for Javascript, not XML.

--- api.js
+++ api.js
@@ -1,10 +1,7 @@
-function KundoAPI(slug, format) {
+function KundoAPI(slug) {
   // Instance variables
-  this.BASE_URL = "http://kundo.se/api";
-  if (format != "json" && format != "xml") {
-    throw "Invalid format type. Please use either json or xml.";
-  }
-  this.format = "." + format;
+  this.BASE_URL = "http://localhost:8000/api";
+  this.FORMAT = ".json";
   if (!slug) {
     throw "Invalid slug. Please provide a proper slug.";
   }
@@ -36,33 +33,33 @@ function KundoAPI(slug, format) {
   // Get data from your forum
   this.all = function(settings) {
     settings = $.extend({}, default_sorted_settings, settings);
-    this.get('/' + this.slug + this.format, settings);
+    this.get('/' + this.slug + this.FORMAT, settings);
   }
   this.single = function(id, settings) {
     if (!id || !typeof id == "number") {
       throw "Invalid id. It should be numeric.";
     }
     settings = $.extend({}, default_plain_settings, settings);
-    this.get('/dialog/' + this.slug + '/' + id + this.format, settings);
+    this.get('/dialog/' + this.slug + '/' + id + this.FORMAT, settings);
   }
   this.comments = function(id, settings) {
     if (!id || !typeof id == "number") {
       throw "Invalid id. It should be numeric.";
     }
     settings = $.extend({}, default_sorted_settings, settings);
-    this.get('/comment/' + this.slug + '/' + id + this.format, settings);
+    this.get('/comment/' + this.slug + '/' + id + this.FORMAT, settings);
   }
   this.topic = function(type, settings) {
     if (type != "q" && type != "p" && type != "s" && type != "b") {
       throw "Invalid topic type. Please use one of: q, p, s, b.";
     }
     settings = $.extend({}, default_sorted_settings, settings);
-    this.get('/' + this.slug + '/' + type + this.format, settings);
+    this.get('/' + this.slug + '/' + type + this.FORMAT, settings);
   }
   this.search = function(query, settings) {
     if (!query) { return; }
     query = decodeURIComponent(query);
     settings = $.extend({}, default_plain_settings, settings);
-    this.get('/search/' + this.slug + '/' + query + this.format, settings);
+    this.get('/search/' + this.slug + '/' + query + this.FORMAT, settings);
   }
 }
